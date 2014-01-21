90d13b6e3334c352632d84bf612c39dbfe9dbd28
karl.m.petersson@gmail.com <karl.m.petersson@gmail.com>
2014-01-21 15:17:30 +0100

    Initial Commit

---
+++ Excercise 1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Excercise 1
+//
+//  Created by Karl Petersson on 2014-01-21.
+//  Copyright (c) 2014 Karl Petersson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
